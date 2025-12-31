d56a0888aef491c8f2a1a77aa05121bae2ac706b
Anthony Cieri <penguinmillion@gmail.com>
2025-12-30 16:00:15 -0800

    feat: unified lights uniform

--- src/rendering/lighting.rs
+++ src/rendering/lighting.rs
@@ -1,4 +1,5 @@
 use crate::rendering::Renderer;
+use bytemuck::Zeroable;
 use cgmath::{
     Point3,
     Vector4,
@@ -116,5 +117,67 @@ impl SunLight
                 contents: bytemuck::cast_slice(lights),
                 usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
             })
+    }
+}
+
+
+
+const MAX_SPHERE_LIGHTS: usize = 16;
+const MAX_SUN_LIGHTS: usize = 2;
+
+
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
+pub struct LightUniform
+{
+    sphere_lights: [SphereLight; MAX_SPHERE_LIGHTS],
+    sphere_light_count: u32,
+    _padding0: [u32; 3],
+    sun_lights: [SunLight; MAX_SUN_LIGHTS],
+    sun_light_count: u32,
+    _padding1: [u32; 3],
+}
+
+
+
+impl LightUniform
+{
+    pub fn new(sphere_lights: &[SphereLight], sun_lights: &[SunLight]) -> Self
+    {
+        let mut sphere_light_uniforms = [SphereLight::zeroed(); MAX_SPHERE_LIGHTS];
+        let mut sphere_count = 0;
+        for light in sphere_lights
+        {
+            if sphere_count >= MAX_SPHERE_LIGHTS
+            {
+                break;
+            }
+
+            sphere_light_uniforms[sphere_count] = light.clone();
+            sphere_count += 1;
+        }
+
+        let mut sun_light_uniforms = [SunLight::zeroed(); MAX_SUN_LIGHTS];
+        let mut sun_count = 0;
+        for light in sun_lights
+        {
+            if sun_count >= MAX_SUN_LIGHTS
+            {
+                break;
+            }
+
+            sun_light_uniforms[sun_count] = light.clone();
+            sun_count += 1;
+        }
+
+        Self {
+            sphere_lights: sphere_light_uniforms,
+            sphere_light_count: sun_count as u32,
+            _padding0: [0; 3],
+            sun_lights: sun_light_uniforms,
+            sun_light_count: sun_count as u32,
+            _padding1: [0; 3],
+        }
     }
 }
